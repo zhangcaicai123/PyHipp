332cd327b9f5760504bb44785828fb0e86fc61c6
shihchengyen <shihcheng@nus.edu.sg>
2021-10-21 00:22:21 +0800

    Revert "Modified path to bash to match that on AWS."
    This reverts commit c62332cacf4f914bb459de95a004891e71c95f7a.

--- ec2snapshot.sh
+++ ec2snapshot.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 ssh -o StrictHostKeyChecking=no -i /data/MyKeyPair.pem ec2-user@xx.xx.xx.xx "source ~/.bash_profile; pcluster update-compute-fleet --status STOP_REQUESTED --region ap-southeast-1 --cluster-name MyCluster01; ~/update_snapshot.sh data 2 MyCluster01"
